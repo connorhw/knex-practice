bca5fe5365111e5db455ef27f975dc2472954083
connorhw <connorwong93@gmail.com>
2020-07-04 16:05:43 -0700

    Need to confirm atLeasty

--- src/drills.js
+++ src/drills.js
@@ -7,6 +7,31 @@ const knexInstance = knex({
 
 console.log('knex and driver installed correctly for drills.js')
 
+/* 
+3. Get all items added after date:
+A function that takes one parameter for daysAgo which will be a number 
+representing a number of days.This function will query the shopping_list
+table using Knex methods and select the rows which have a date_added 
+that is greater than the daysAgo.
+*/
+function atLeastXDaysAgo(daysAgo) {
+    knexInstance
+        .select('name', 'date_added')
+        .from('shopping_list')
+        .where(
+            'date_added',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+        )
+        .orderBy([
+            { column: 'date_added', order: 'DESC' },
+        ])
+        .then(result => {
+            console.log(result)
+        })
+}
+atLeastXDaysAgo(7)
+/*
 function paginatePage(pageNumber) {
     const perPage = 6
     const offset = perPage * (pageNumber - 1)
@@ -20,6 +45,7 @@ function paginatePage(pageNumber) {
         })
 }
 paginatePage(5)
+*/
 
 /*
 function searchByTerm(searchTerm) {
