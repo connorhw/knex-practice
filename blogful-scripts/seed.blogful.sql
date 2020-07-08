0fa318d6057f91790630673bdf017d93d1862ffb
connorhw <connorwong93@gmail.com>
2020-07-08 02:33:53 -0700

    started blogful, created articles table and seeded it.

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Ellen Gets Into Nursing School', now() - '21 days'::INTERVAL, 'content1'),
+    ('Connor Take A Coding Bootcamp', now() - '20 days'::INTERVAL, 'content2'),
+    ('Ruby Learns to Swim', now() - '19 days'::INTERVAL, 'content3'),
+    ('10 Habits of Successful People', now() - '18 days'::INTERVAL, 'content4'),
+    ('How I Got My Dream Job', now() - '17 days'::INTERVAL, 'content5'),
+    ('Going Against the Grain', now() - '16 days'::INTERVAL, 'content6'),
+    ('To New Beginnings', now() - '15 days'::INTERVAL, 'content7'),
+    ('Apple Moves to ARM Processors', now() - '14 days'::INTERVAL, 'content8'),
+    ('I Want To Invest More', now() - '13 days'::INTERVAL, 'content9'),
+    ('Your Workspace And Your Productivity', now() - '12 days'::INTERVAL, 'content10'),
+    ('Why the 2019 16 Inch Macbook Pro is the Best by Apple', now() - '11 days'::INTERVAL, 'content11'),
+    ('Chances the NBA Restart is a Disaster', now() - '10 days'::INTERVAL, 'content12'),
+    ('Learn to use things and love people', now() - '9 days'::INTERVAL, 'content13'),
+    ('Talk with people who disagree with you', now() - '8 days'::INTERVAL, 'content14'),
+    ('What 2020 will be seen as 50 years from now', now() - '7 days'::INTERVAL, 'content15'),
+    ('Title 16', now() - '6 days'::INTERVAL, 'content16'),
+    ('Title 17', now() - '5 days'::INTERVAL, 'content17'),
+    ('Title 18', now() - '4 days'::INTERVAL, 'content18'),
+    ('Title 19', now() - '3 days'::INTERVAL, 'content19'),
+    ('Title 20', now() - '2 days'::INTERVAL, 'content20')
+;
